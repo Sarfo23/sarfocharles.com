fe093ce57e5a6967a709126e3e64c2eb2689ffb9
Sarfo23 <112715605+Sarfo23@users.noreply.github.com>
2025-12-02 07:17:07 -0800

    Fix: Make header navigation responsive on mobile

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ projectButtons.forEach(button => {
 
 // Hamburger Menu
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('header nav ul');
+const navMenu = document.querySelector('header nav');
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
